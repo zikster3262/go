fa9517c0f733998577b68da93555cbe01be16cbc
David Tran <zikster3262@gmail.com>
2020-09-05 11:38:42 +0200

    delete getTitle validator func

--- github.com/zikster3262/web-page/main.go
+++ github.com/zikster3262/web-page/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -36,15 +35,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
